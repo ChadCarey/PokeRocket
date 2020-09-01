d75b60c96005fb51c7c5162eb874101f03ec5fef
ChadCarey <chadcarey7@gmail.com>
2020-08-31 19:48:04 -0500

    cleaned up and refactored the project

--- pokerocket_automation/camel/src/main/java/com/chad/portfolio/pokerocket/camel/routebuilders/PokeApiSyncRoutes.java
+++ pokerocket_automation/camel/src/main/java/com/chad/portfolio/pokerocket/camel/routebuilders/PokeApiSyncRoutes.java
@@ -3,6 +3,8 @@ package com.chad.portfolio.pokerocket.camel.routebuilders;
 //import com.chad.portfolio.pokerocket.model.Pokemon;
 
 import com.chad.portfolio.pokerocket.PokeApiSync;
+import com.chad.portfolio.pokerocket.camel.SystemProperties;
+import com.chad.portfolio.pokerocket.camel.exceptions.SystemLevelException;
 import com.chad.portfolio.pokerocket.camel.factories.EndpointFactory;
 import com.chad.portfolio.pokerocket.clients.pokeapi.PokeApiProxy;
 import com.chad.portfolio.pokerocket.clients.pokeapi.PokeApiProxyFactory;
@@ -19,48 +21,65 @@ public class PokeApiSyncRoutes implements RoutesBuilder {
 
     private static Logger log = LoggerFactory.getLogger(PokeApiSyncRoutes.class);
 
-    private final static String pokeApiUrl = "https://pokeapi.co/api/v2";
-    private final static String pokerocketEndpointsUrl = "http://172.19.0.3:5000";
+    protected static final Integer MIN_POLL_PERIOD = 5000;
 
     private PokeApiSync pokeSync;
     private PokeApiProxy pokeApiProxy;
     private PokeRocketEndpointsProxy pokeRocketEndpointsProxy;
 
-    public PokeApiSyncRoutes() {
-        pokeApiProxy = PokeApiProxyFactory.Create(pokeApiUrl);
-        pokeRocketEndpointsProxy = PokeRocketEndpointsProxyFactory.Create(pokerocketEndpointsUrl);
+    private String timerEndpoint;
+
+
+    public PokeApiSyncRoutes(String pokeApiUrl, String pokeRocketEndpointUrl, PollProperties pollProperties) throws SystemLevelException {
+
+        validatePollParameters(pollProperties);
+
+        this.timerEndpoint = EndpointFactory.SimpleTimer(
+            pollProperties.getPollPeriod(),
+            pollProperties.getPollDelay(),
+            pollProperties.getPollRepeatCount()
+        );
+
+        this.pokeApiProxy = PokeApiProxyFactory.Create(pokeApiUrl);
+        this.pokeRocketEndpointsProxy = PokeRocketEndpointsProxyFactory.Create(pokeRocketEndpointUrl);
+
         this.pokeSync = new PokeApiSync(new StatefullNextPokemonResolver(), pokeApiProxy, pokeRocketEndpointsProxy);
+
     }
+
 
     @Override
     public void addRoutesToCamelContext(CamelContext camelContext) throws Exception {
         camelContext.addRoutes(newPokeApiSyncRoutesRoute());
     }
 
+
     private RouteBuilder newPokeApiSyncRoutesRoute() {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
 
-//                SystemProperties properties = SystemProperties.getInstance();
-            //    String onFailToEndpoint = EndpointFactory.SedaPublishQueue(RouteConstants.EVENT_FAIL_QUEUE);
-
-                String timerEndpoint = EndpointFactory.SimpleTimer(
-                        10000,1,10
-                );
-//                       properties.getPokeApiPollPeriod(),
-//                       properties.getPokeApiPollDelay(),
-//                       properties.getPokeApiPollRepeatCount());
-
-//                 onException(Exception.class).handled(false).maximumRedeliveries( properties.getPokeRocketMaxRetries() ).delay(properties.getPokeRocketDelayBeforeRetry())
-//                         .bean(ExchangeMessageAdapters.class, "eventMessageExchangeToEventSubscriptionFail").marshal(DataFormats.EVENT_FAIL_FORMAT)
-//                         .to(onFailToEndpoint);
+                // TODO: finish retries and fail queue
+                // String onFailToEndpoint = EndpointFactory.SedaPublishQueue(RouteConstants.EVENT_FAIL_QUEUE);
+                // onException(Exception.class).handled(false).maximumRedeliveries( properties.getPokeRocketMaxRetries() ).delay(properties.getPokeRocketDelayBeforeRetry())
+                //         .bean(ExchangeMessageAdapters.class, "NewPokeSyncFail").marshal(DataFormats.MESSAGE_FAIL_FORMAT)
+                //         .to(onFailToEndpoint);
 
                 from(timerEndpoint)
                     .log("Running timer")
                     .process(exchange -> pokeSync.syncNext())
                 .end();
             }
+
         };
     }
+
+
+    private void validatePollParameters(PollProperties pollProperties) throws SystemLevelException {
+        if(pollProperties.getPollPeriod() < MIN_POLL_PERIOD) {
+            // polling too often could get us banned from using PokeApi
+            throw new SystemLevelException("pollPeriod < MIN_POLL_PERIOD. pollPeriod = "+pollProperties.getPollPeriod()+"\n");
+        }
+    }
+
 }
